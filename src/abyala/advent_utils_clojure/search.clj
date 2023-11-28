115ce5f3791531de1ab94d9b8dfa29e97e8c004a
Andrew Byala <abyala@yahoo.com>
2023-11-28 10:53:42 -0600

    Added stateful searches.
    Refactored both breadth-first and depth-first searches, stateful and stateless,
    to use common logic.

--- src/abyala/advent_utils_clojure/search.clj
+++ src/abyala/advent_utils_clojure/search.clj
@@ -1,40 +1,15 @@
 (ns abyala.advent-utils-clojure.search)
 
-(defn- no-next-vals-fn [_] nil)
+(defn- breadth-first-append-values [old-vals new-vals]
+  (concat old-vals new-vals))
+(defn- depth-first-append-values [old-vals new-vals]
+  (concat new-vals old-vals))
 
-(defn breadth-first
-  "Performs a breadth first search, returning the first value that satisfies the predicate `pred`. The function will
-  go through the values in `initial-vals` in order. For each value that fails the `pred`, the optional `next-vals-fn`
-  will be invoked to identify additional values to add to the end of the initial-vals queue."
-
-  ([initial-vals pred]
-   (breadth-first initial-vals no-next-vals-fn pred))
-
-  ([initial-vals next-vals-fn pred]
-   (cond
-     ; Validate initial-vals
-     (nil? initial-vals) nil
-     (not (coll? initial-vals)) (recur (list initial-vals) next-vals-fn pred)
-
-     ; Validate next-vals-fn
-     (nil? next-vals-fn) (recur initial-vals no-next-vals-fn pred)
-
-     ; Good to go
-     :else (loop [vals (seq initial-vals)]
-             (when (seq vals)
-               (let [[v & v'] vals]
-                 (if (pred v)
-                   v
-                   (recur (concat v' (next-vals-fn v))))))))))
-
-(defn depth-first [initial-vals next-vals-fn pred]
+(defn- stateless-search [append-vals-fn initial-vals next-vals-fn pred]
   (cond
-    ;Validate initial-vals
+    ; Validate initial-vals
     (nil? initial-vals) nil
-    (not (coll? initial-vals)) (recur (list initial-vals) next-vals-fn pred)
-
-    ;Validate next-vals-fn
-    (nil? next-vals-fn) (recur initial-vals no-next-vals-fn pred)
+    (not (coll? initial-vals)) (recur append-vals-fn (list initial-vals) next-vals-fn pred)
 
     ; Good to go
     :else (loop [vals (seq initial-vals)]
@@ -42,4 +17,63 @@
               (let [[v & v'] vals]
                 (if (pred v)
                   v
-                  (recur (concat (next-vals-fn v) v'))))))))
+                  (recur (append-vals-fn v' (next-vals-fn v)))))))))
+
+(defn breadth-first
+  "Performs a breadth first search, returning the first value that satisfies the predicate `pred`. The function will
+  go through the values in `initial-vals` in order. For each value that fails the `pred`, the `next-vals-fn`
+  will be invoked to identify additional values, if any, to add to the end of the initial-vals queue. `next-vals-fn`
+  is expected to take in a single argument for the current value being evaluated."
+
+  [initial-vals next-vals-fn pred]
+  (stateless-search breadth-first-append-values initial-vals next-vals-fn pred))
+
+
+(defn depth-first
+  "Performs a depth-first search, returning the first value that satisfies the predicate `pred`. The function will
+  go through the values in `initial-vals` in order. For each value that fails the `pred`, the `next-vals-fn`
+  will be invoked to identify additional values, if any, to add to the front of the initial-vals queue. `next-vals-fn`
+  is expected to take in a single argument for the current value being evaluated."
+
+  [initial-vals next-vals-fn pred]
+  (stateless-search depth-first-append-values initial-vals next-vals-fn pred))
+
+(defrecord DoneSearching [value])
+(defrecord KeepSearching [next-state next-vals])
+
+(defn done-searching
+  "Invoked within the `eval-fn` of a stateful search to demonstrate that the search has found its result, `v`."
+  [v] (->DoneSearching v))
+(defn keep-searching
+  "Invoked within the `eval-fn` of a stateful search to demonstrate that the search must continue. The `next-state`
+  argument is the state to pass to the next value being evaluated. The optional `next-vals` returns a collection of
+  values to add to the search space."
+  ([next-state] (->KeepSearching next-state ()))
+  ([next-state next-vals]
+   (assert ((some-fn nil? coll?) next-vals) "next-vals must be either nil or a collection")
+   (->KeepSearching next-state next-vals)))
+
+(defn- stateful-search [append-vals-fn initial-state initial-vals eval-fn]
+  (loop [vals (seq initial-vals), state initial-state]
+    (when (seq vals)
+      (let [[v & v'] vals
+            result (eval-fn state v)]
+        (cond
+          (instance? DoneSearching result) (:value result)
+          (instance? KeepSearching result) (recur (append-vals-fn v' (:next-vals result))
+                                                  (:next-state result))
+          :else (throw (IllegalArgumentException. "Evaluation function must return either DoneSearching or KeepSearching")))))))
+
+(defn breadth-first-stateful
+  "Similar to [[breadth-first]], this performs a breadth-first search but retains state between evaluations of searched
+  values. The `eval-fn` must take two arguments, the `state` and the `value` being considered, and its return must
+  call either [[done-searching]] or [[keep-searching]]. "
+  [initial-state initial-vals eval-fn]
+  (stateful-search breadth-first-append-values initial-state initial-vals eval-fn))
+
+(defn depth-first-stateful
+  "Similar to [[depth-first]], this performs a depth-first search but retains state between evaluations of searched
+  values. The `eval-fn` must take two arguments, the `state` and the `value` being considered, and its return must
+  call either [[done-searching]] or [[keep-searching]]. "
+  [initial-state initial-vals eval-fn]
+  (stateful-search depth-first-append-values initial-state initial-vals eval-fn))
--- test/abyala/advent_utils_clojure/search_test.clj
+++ test/abyala/advent_utils_clojure/search_test.clj
@@ -2,36 +2,28 @@
   (:require [clojure.test :refer :all]
             [abyala.advent-utils-clojure.search :as s]))
 
-(def incr-one-fn #(vector (inc %)))
-(def mod-10? #(zero? (mod % 10)))
+(defn no-next-vals [_] nil)
+(defn incr-vec [n] [(inc n)])
+(defn mod-10? [n] (zero? (mod n 10)))
 
 (deftest breadth-first-test
   (testing "Normal input"
-    (are [input expected] (= expected (s/breadth-first input incr-one-fn mod-10?))
+    (are [input expected] (= expected (s/breadth-first input incr-vec mod-10?))
                           [6] 10
                           [5] 10
                           [10] 10
                           [5 6] 10
                           (list 5 6) 10))
   (testing "Scalar input"
-    (are [input expected] (= expected (s/breadth-first input incr-one-fn mod-10?))
+    (are [input expected] (= expected (s/breadth-first input incr-vec mod-10?))
                           6 10
                           11 20))
-  (testing "No next-vals-fn"
-    (are [input] (nil? (s/breadth-first input nil mod-10?))
-                 [5]
-                 6)
-    (are [input expected] (= expected (s/breadth-first input mod-10?))
-                          (range) 0
-                          (range 1 20) 10
-                          [10] 10
-                          (list 9 10 11) 10))
 
   (testing "Nil next-vals-fn"
-    (are [input] (nil? (s/breadth-first input nil mod-10?))
+    (are [input] (nil? (s/breadth-first input no-next-vals mod-10?))
                  [5]
                  6)
-    (are [input expected] (= expected (s/breadth-first input nil mod-10?))
+    (are [input expected] (= expected (s/breadth-first input no-next-vals mod-10?))
                           (range) 0
                           (range 1 20) 10
                           [10] 10
@@ -43,24 +35,37 @@
                           [9 4] 10
                           [8 4] 10)))
 
+(deftest stateful-tests
+  ; Take turns of going either L or R at an intersection. Return the first decision point after making 5 turns.
+  ; Breadth-first: L, R, LL, LR, RL
+  ; Depth-first: L, LL, LLL, LLLL, LLLLL
+  (let [left-and-right [\L \R]
+        next-turns (fn [path] (map #(str path %) left-and-right))]
+    (are [alg expected] (= expected (alg [] left-and-right (fn [acc path]
+                                                             (if (= (count acc) 5)
+                                                               (s/done-searching (last acc))
+                                                               (s/keep-searching (conj acc path) (next-turns path))))))
+                        s/breadth-first-stateful "RL"
+                        s/depth-first-stateful "LLLLL")))
+
 (deftest depth-first-test
   (testing "Normal input"
-    (are [input expected] (= expected (s/depth-first input incr-one-fn mod-10?))
+    (are [input expected] (= expected (s/depth-first input incr-vec mod-10?))
                           [6] 10
                           [5] 10
                           [10] 10
                           [5 6] 10
                           (list 5 6) 10))
   (testing "Scalar input"
-    (are [input expected] (= expected (s/depth-first input incr-one-fn mod-10?))
+    (are [input expected] (= expected (s/depth-first input incr-vec mod-10?))
                           6 10
                           11 20))
 
-  (testing "Nil next-vals-fn"
-    (are [input] (nil? (s/depth-first input nil mod-10?))
+  (testing "Using no-next-vals-fn"
+    (are [input] (nil? (s/depth-first input no-next-vals mod-10?))
                  [5]
                  6)
-    (are [input expected] (= expected (s/depth-first input nil mod-10?))
+    (are [input expected] (= expected (s/depth-first input no-next-vals mod-10?))
                           (range) 0
                           (range 1 20) 10
                           [10] 10
@@ -73,6 +78,6 @@
                           [8 4] 10)))
 
 (deftest compare-algorithms
-  (are [alg input expected] (= expected (alg input incr-one-fn mod-10?))
+  (are [alg input expected] (= expected (alg input incr-vec mod-10?))
                             s/breadth-first [5 19] 20
                             s/depth-first [5 19] 10))
