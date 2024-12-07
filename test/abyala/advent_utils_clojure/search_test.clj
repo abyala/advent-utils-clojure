5e597821b4f83cfe0fbf5333bc9c2fed00d10a49
Andrew Byala <abyala@yahoo.com>
2024-12-07 01:36:39 -0600

    Minor fixes to test suite

--- test/abyala/advent_utils_clojure/search_test.clj
+++ test/abyala/advent_utils_clojure/search_test.clj
@@ -8,14 +8,14 @@
 
 (deftest breadth-first-test
   (testing "Normal input"
-    (are [input expected] (= expected (s/breadth-first input incr-vec mod-10?))
+    (are [input expected] (= (s/breadth-first input incr-vec mod-10?) expected)
                           [6] 10
                           [5] 10
                           [10] 10
                           [5 6] 10
                           (list 5 6) 10))
   (testing "Scalar input"
-    (are [input expected] (= expected (s/breadth-first input incr-vec mod-10?))
+    (are [input expected] (= (s/breadth-first input incr-vec mod-10?) expected)
                           6 10
                           11 20))
 
@@ -23,17 +23,17 @@
     (are [input] (nil? (s/breadth-first input no-next-vals mod-10?))
                  [5]
                  6)
-    (are [input expected] (= expected (s/breadth-first input no-next-vals mod-10?))
+    (are [input expected] (= (s/breadth-first input no-next-vals mod-10?) expected)
                           (range) 0
                           (range 1 20) 10
                           [10] 10
                           (list 9 10 11) 10))
 
   (testing "Multiple values in next-vals-fn"
-    (are [input expected] (= expected (s/breadth-first input #(vector (inc %) (+ 6 %)) mod-10?))
+    (are [input expected] (= (s/breadth-first input #(vector (inc %) (+ 6 %)) mod-10?) expected)
                           [10 4] 10
-                          [9 4] 10
-                          [8 4] 10)))
+                          [9 14] 10
+                          [8 14] 20)))
 
 (deftest stateful-tests
   ; Take turns of going either L or R at an intersection. Return the first decision point after making 5 turns.
@@ -41,23 +41,24 @@
   ; Depth-first: L, LL, LLL, LLLL, LLLLL
   (let [left-and-right [\L \R]
         next-turns (fn [path] (map #(str path %) left-and-right))]
-    (are [alg expected] (= expected (alg [] left-and-right (fn [acc path]
+    (are [alg expected] (= (alg [] left-and-right (fn [acc path]
                                                              (if (= (count acc) 5)
                                                                (s/done-searching (last acc))
-                                                               (s/keep-searching (conj acc path) (next-turns path))))))
+                                                               (s/keep-searching (conj acc path) (next-turns path)))))
+                           expected)
                         s/breadth-first-stateful "RL"
                         s/depth-first-stateful "LLLLL")))
 
 (deftest depth-first-test
   (testing "Normal input"
-    (are [input expected] (= expected (s/depth-first input incr-vec mod-10?))
+    (are [input expected] (= (s/depth-first input incr-vec mod-10?) expected)
                           [6] 10
                           [5] 10
                           [10] 10
                           [5 6] 10
                           (list 5 6) 10))
   (testing "Scalar input"
-    (are [input expected] (= expected (s/depth-first input incr-vec mod-10?))
+    (are [input expected] (= (s/depth-first input incr-vec mod-10?) expected)
                           6 10
                           11 20))
 
@@ -65,19 +66,19 @@
     (are [input] (nil? (s/depth-first input no-next-vals mod-10?))
                  [5]
                  6)
-    (are [input expected] (= expected (s/depth-first input no-next-vals mod-10?))
+    (are [input expected] (= (s/depth-first input no-next-vals mod-10?) expected)
                           (range) 0
                           (range 1 20) 10
                           [10] 10
                           (list 9 10 11) 10))
 
   (testing "Multiple values in next-vals-fn"
-    (are [input expected] (= expected (s/depth-first input #(vector (inc %) (+ 6 %)) mod-10?))
+    (are [input expected] (= (s/depth-first input #(vector (inc %) (+ 6 %)) mod-10?) expected)
                           [10 4] 10
                           [9 4] 10
                           [8 4] 10)))
 
 (deftest compare-algorithms
-  (are [alg input expected] (= expected (alg input incr-vec mod-10?))
+  (are [alg input expected] (= (alg input incr-vec mod-10?) expected)
                             s/breadth-first [5 19] 20
                             s/depth-first [5 19] 10))
